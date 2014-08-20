d55ce00a55815f82ec04fcbd16a103355f6ca8b4
Warren Haskins <warrenhaskins@gmail.com>
2014-08-20 10:21:21 +0100

    Added Comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-var gulp       = require('gulp'),
-     less      = require('gulp-less'),
-     cssmin    = require('gulp-minify-css'),
-     notify    = require('gulp-notify'),
-     path      = require('path'),
-     uglify    = require('gulp-uglify'),
-     concat    = require('gulp-concat'),
-     rename    = require('gulp-rename'),
-     livereload = require('gulp-livereload');
+var 	gulp       	= require('gulp'),
+     less      	= require('gulp-less'),
+     cssmin    	= require('gulp-minify-css'),
+     notify    	= require('gulp-notify'),
+     path      	= require('path'),
+     uglify    	= require('gulp-uglify'),
+     concat    	= require('gulp-concat'),
+     rename    	= require('gulp-rename'),
+     livereload 	= require('gulp-livereload'); 
+
+
+/*
+|--------------------------------------------------------------------------
+| Compile Less, Minify and Save
+|--------------------------------------------------------------------------
+*/
 
 gulp.task('less', function() {
      return gulp.src('app/public/less/style.less')
@@ -16,6 +23,13 @@ gulp.task('less', function() {
           .pipe(notify({ message: 'Less - Done!'}));
 });
 
+
+/*
+|--------------------------------------------------------------------------
+| Compile JS, Minify and Save
+|--------------------------------------------------------------------------
+*/
+
 gulp.task('uglify', function() {
      return gulp.src('app/public/js/src/*.js')
           .pipe(concat('main.js'))
@@ -24,6 +38,13 @@ gulp.task('uglify', function() {
           .pipe(gulp.dest('app/public/js'))
           .pipe(notify({ message: 'JS - Done!' }))
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| Watch
+|--------------------------------------------------------------------------
+*/
 
 gulp.task('watch', function() {
      gulp.watch('app/public/less/*.less', ['less']);
